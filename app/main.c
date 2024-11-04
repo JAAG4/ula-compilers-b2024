e969cb493d622fbafdb00be149ee9479248254d9
JAAG4 <45705167+JAAG4@users.noreply.github.com>
2024-11-03 20:28:13 -0400

    Validation

---
+++ app/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern FILE *yyin;
+extern int yyparse();
+// extern int parser_result;
+void usage(char *argv[])
+{
+    printf("Usage: %s input_file\n", argv[0]);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        usage(argv);
+    }
+
+    yyin = fopen(argv[1], "r");
+
+    if (!yyin)
+    {
+        printf("Could not open %s\n", argv[1]);
+        exit(1);
+    }
+
+    int result = yyparse();
+
+    if (result == 0)
+    {
+        printf("✅\tParsed!\t✅\nResult: %d\n", result);
+    }
+    else
+    {
+        printf("❌❌\tParse failed!\t❌❌\n");
+    }
+
+    return 0;
+}
